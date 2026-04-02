576a467a102463c4763f94bf31c5b9ff66714338
danielbotros <daniel.botros@viam.com>
2026-04-02 10:57:42 -0400

    Add test for webrtc request tracing

--- examples/customresources/demos/multiplemodules/moduletest/module_test.go
+++ examples/customresources/demos/multiplemodules/moduletest/module_test.go
@@ -20,6 +20,7 @@ import (
 	"go.viam.com/utils/rpc"
 	gtestutils "go.viam.com/utils/testutils"
 
+	"go.viam.com/rdk/components/motor"
 	"go.viam.com/rdk/config"
 	"go.viam.com/rdk/examples/customresources/apis/gizmoapi"
 	"go.viam.com/rdk/examples/customresources/apis/summationapi"
@@ -154,6 +155,91 @@ func TestMultipleModules(t *testing.T) {
 		retAdd, err := add.Sum(context.Background(), nums)
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, retAdd, test.ShouldEqual, 22.5)
+	})
+}
+
+// TestWebRTCSpans verifies that gRPC calls made over WebRTC connections generate
+// spans. This is a regression test for a bug where the otelgrpc stats handler was
+// only wired to the direct gRPC server, not the WebRTC server, so WebRTC calls
+// produced no spans at all.
+func TestWebRTCSpans(t *testing.T) {
+	logger, observer := logging.NewObservedTestLogger(t)
+	testViamHome := t.TempDir()
+	var port int
+	success := false
+	for portTryNum := 0; portTryNum < 10; portTryNum++ {
+		portLocal, err := goutils.TryReserveRandomPort()
+		test.That(t, err, test.ShouldBeNil)
+		port = portLocal
+
+		cfgJSON := fmt.Sprintf(`{
+			"components": [{"name": "motor-1", "type": "motor", "model": "fake"}],
+			"network": {"bind_address": "localhost:%d"},
+			"tracing": {"enabled": true, "disk": true}
+		}`, port)
+		cfgFile, err := os.CreateTemp(t.TempDir(), "viam-test-config-*")
+		test.That(t, err, test.ShouldBeNil)
+		_, err = cfgFile.WriteString(cfgJSON)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, cfgFile.Close(), test.ShouldBeNil)
+
+		server := robottestutils.ServerAsSeparateProcess(t, cfgFile.Name(), logger,
+			robottestutils.WithViamHome(testViamHome))
+
+		err = server.Start(context.Background())
+		test.That(t, err, test.ShouldBeNil)
+
+		if robottestutils.WaitForServing(observer, port) {
+			success = true
+			defer func() {
+				test.That(t, server.Stop(), test.ShouldBeNil)
+			}()
+			break
+		}
+		server.Stop()
+	}
+	test.That(t, success, test.ShouldBeTrue)
+
+	connectCtx, cancelConn := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancelConn()
+	var rc robot.Robot
+	for {
+		dialCtx, dialCancel := context.WithTimeout(connectCtx, time.Millisecond*500)
+		var err error
+		rc, err = client.New(dialCtx, fmt.Sprintf("localhost:%d", port), logger,
+			client.WithDisableSessions(),
+		)
+		dialCancel()
+		if !errors.Is(err, context.DeadlineExceeded) {
+			test.That(t, err, test.ShouldBeNil)
+			break
+		}
+		select {
+		case <-connectCtx.Done():
+			t.Fatal(connectCtx.Err())
+		default:
+		}
+	}
+	defer func() {
+		test.That(t, rc.Close(context.Background()), test.ShouldBeNil)
+	}()
+
+	t.Run("Test WebRTC spans", func(t *testing.T) {
+		m, err := motor.FromProvider(rc, "motor-1")
+		test.That(t, err, test.ShouldBeNil)
+		_, err = m.IsMoving(context.Background())
+		test.That(t, err, test.ShouldBeNil)
+
+		gtestutils.WaitForAssertionWithSleep(t, time.Millisecond*500, 20, func(t testing.TB) {
+			checkTraceContents(t, testViamHome,
+				spanExpectation{
+					service:    "rdk",
+					kind:       otlpv1.Span_SPAN_KIND_SERVER,
+					rpcService: "viam.component.motor.v1.MotorService",
+					rpcMethod:  "IsMoving",
+				},
+			)
+		})
 	})
 }
 
