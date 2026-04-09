1afdc628c5e35809c626e49b4c955d0f229e6a65
Daniel Botros <93689852+danielbotros@users.noreply.github.com>
2026-04-09 12:57:58 -0400

    remove newline

--- robot/web/web.go
+++ robot/web/web.go
@@ -717,7 +717,6 @@ func (svc *webService) initAuthHandlers(listenerTCPAddr *net.TCPAddr, options we
 				authEntities = addIfNotFound(weboptions.LocalHostWithPort(listenerTCPAddr))
 			}
 		}
-
 		if options.Secure && len(options.Auth.TLSAuthEntities) != 0 {
 			rpcOpts = append(rpcOpts, rpc.WithTLSAuthHandler(options.Auth.TLSAuthEntities))
 		}
