3edf16fb3ea3ed6203812dceca874b3f603159a5
danielbotros <daniel.botros@viam.com>
2026-04-14 14:54:11 -0400

    Bump sleep

--- data/collector_test.go
+++ data/collector_test.go
@@ -153,9 +153,9 @@ func TestSuccessfulWrite(t *testing.T) {
 			// We need to avoid adding time until after the underlying goroutine has started sleeping.
 			// If we add time before that point, data will never be captured, because time will never be greater than
 			// the initially calculated time.
-			// Sleeping for 10ms is a hacky way to ensure that we don't encounter this situation. It gives 10ms
+			// Sleeping for 100ms is a hacky way to ensure that we don't encounter this situation. It gives 100ms
 			// for those few sequential lines in collector.go to execute, so that that occurs before we add time below.
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(100 * time.Millisecond)
 			for i := 0; i < tc.expectReadings; i++ {
 				mockClock.Add(params.Interval)
 				select {
